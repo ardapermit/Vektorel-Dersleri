8e2b663789d27cd8cbdb697ac8af038d166da694
ardapermit <arda.qway@gmail.com>
2025-05-03 14:24:22 +0300

    Ders hafta bir çalışmaları

---
+++ Newest.py
@@ -0,0 +1,9 @@
+xxx = "Vektörel bilişim"
+print(xxx) 
+print(xxx(xxx[2])# index=indis=adres/konum
+print(xxx[2:])    #  2 den itibaren
+print(xxx[2:7])      #  2-7 arası
+print(xxx[2:15:3])       # 1-15 arası 3'e
+print(xxx[:5]) # 5'e kadar
+print(xxx[-3:]) # sondan index    
+
